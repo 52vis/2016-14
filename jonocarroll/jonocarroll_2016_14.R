221764c85cfb6515e0cb57f257f95dd1fd9c8ec6
Jonathan Carroll <jono@jcarroll.com.au>
2016-04-08 23:54:54 +0930

    data in data.frame form

--- jonocarroll/jonocarroll_2016_14.R
+++ jonocarroll/jonocarroll_2016_14.R
@@ -5,13 +5,17 @@
 ## github: github.com/jonocarroll/2016-14
 
 ## load relevant packages
-pacman::p_load(magrittr, dplyr, tidyr, ggplot2, httr, readxl)
+pacman::p_load(magrittr, dplyr, tidyr, ggplot2, httr, readxl, purrr, data.table)
 
 setwd("jonocarroll")
 
 ## load the data from hudexchange.info (download once)
 # URL <- "https://www.hudexchange.info/resources/documents/2007-2015-PIT-Counts-by-CoC.xlsx"
 # GET(URL, write_disk("homeless.xlsx", overwrite=TRUE))
-HUDdata <- read_excel("homeless.xlsx")
 
+## load the sheets into a list of data.frames
+HUDdata <- map(as.character(2007:2015), ~read_excel("homeless.xlsx", sheet=.x)) %>%
+  map(function(x) mutate(x, Year=sub(".*, ","",names(x)[3]))) %>%
+  map(function(x) setNames(x, sub("(.*),.*","\\1",names(x))))
 
+HUDdataDF <- rbindlist(HUDdata, fill=TRUE)
