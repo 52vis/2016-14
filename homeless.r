da40fceb437c71b7837d58f211742f9bd178b13c
Jake Kaupp <jkaupp@gmail.com>
2016-04-11 15:59:03 -0400

    Spin functionality and images

--- homeless.r
+++ homeless.r
@@ -9,7 +9,6 @@ library(grid)
 library(ggalt)
 library(ggrepel)
 
-
 # grab the HUD homeless data
 URL <- "https://www.hudexchange.info/resources/documents/2007-2015-PIT-Counts-by-CoC.xlsx"
 fil <- basename(URL)
@@ -55,7 +54,7 @@ states <- homeless %>%
          homeless_veterans_per_100k = (unsheltered_homeless/population)*100000)
 
 
-#Load in COC funding data
+# Load in COC funding data
 coc_funding <- read_excel(list.files(".", full.names = TRUE, pattern = "CPD-Awards")) %>%
   set_names(tolower(names(.)))
 
@@ -66,18 +65,25 @@ state_funding <- coc_funding %>%
   filter(year >= 2007)
 
 states <- left_join(states,state_funding) %>%
-  mutate(funding = (total_funding/population)*100000) %>%
+  mutate(funding_per_100k = (total_funding/population)*100000) %>%
   filter(year != "2015") %>%
   group_by(state, year) %>%
   arrange
 
-nudges <- data_frame(nudge_x = c(0, 0, 0, 100000, -10000000, 10000000, -10000000, 100000), nudge_y = c(-2500, -2500, -2500, 2500, 0, 0, -2500, -2500))
+group_by(states, name) %>%
+  summarise(mean=mean(funding_per_100k, na.rm=TRUE)) %>%
+  arrange(desc(mean)) -> ordr
 
-plot_data <- states %>%
+
+
+
+nudges <- data_frame(nudge_x = c(0, 0, 0, 100000, -10000000, 10000000, -10000000, 100000), nudge_y = c(-2500, -2500, -2500, 3500, 0, 0, -2500, -2500))
+
+national <- states %>%
   group_by(year) %>%
   summarize_each(
     funs(sum(., na.rm = TRUE)),
-    funding,
+    funding_per_100k,
     total_funding,
     total_homeless,
     total_homeless_per_100k,
@@ -86,18 +92,24 @@ plot_data <- states %>%
   ) %>%
   bind_cols(nudges)
 
+states$name <- factor(states$name, levels=ordr$name)
+
+
 
 # Epoca temporal scatterplot
-gg <- ggplot(plot_data, aes(x = total_funding, y = total_homeless))
+# All credit for this style of plot goes to Epoca Magzine & Alberto Cairo
+# Cairo, Alberto. The Functional Art: An introduction to information graphics and visualization. New Riders, 2012.
+# National Level
+#+ national fig.retina=2, fig.width=12, fig.height=8
+gg <- ggplot(national, aes(x = total_funding, y = total_homeless))
 gg <- gg + ggalt:::geom_xspline2(aes(s_open = TRUE, s_shape = 0.6, size = 8))
 gg <- gg + geom_point(aes(group = year), size = 3, pch = 21, fill = "black", color = "white")
 gg <- gg + geom_text(aes(label = year, y = total_homeless + nudge_y, x = total_funding + nudge_x), family = "Oswald Light")
 gg <- gg + scale_x_continuous(labels = scales::dollar, limits = c(1300000000,1790000000))
 gg <- gg + scale_y_continuous(labels = scales::comma, limits = c(565000, 650000))
-#gg <- gg + facet_wrap(~name, scales="free", ncol=6)
 gg <- gg + labs(x = NULL, y = NULL,
-                title="Temporal Scatterplot of US Department of Housing & Urban Development (HUD) Unsheltered (Estimated) Homeless Population contrasted with HUD Continuum of Care Program (CoC) Funding",
-                subtitle="Year aggregates calculated from HUD Communities of Care Regional Surveys and CPD Allocations and Awards",
+                title="Temporal Scatterplot of US Department of Housing & Urban Development (HUD) Unsheltered (Estimated) Homeless Population\ncontrasted with HUD Continuum of Care Program (CoC) Funding",
+                subtitle="Aggregates calculated from HUD Communities of Care Regional Surveys and CPD Allocations and Awards",
                 caption="\nHomeless population data from: https://www.hudexchange.info/resource/4832/2015-ahar-part-1-pit-estimates-of-homelessness/\nCoC program Funding data from: https://www.hudexchange.info/grantees/cpd-allocations-awards/")
 
 gg <- gg + theme(text = element_text(family = "Oswald Light", size = 12, color = "grey20"))
@@ -118,7 +130,140 @@ gg <- gg + theme(strip.background = element_blank())
 gg <- gg + theme(plot.background = element_blank())
 gg <- gg + theme(plot.subtitle = element_text(hjust = 0))
 gg <- gg + theme(plot.caption = element_text(hjust = 1, size = 8, vjust = 1))
+gg <- gg + theme(plot.margin=margin(10, 20, 10, 15))
 
-gg
+
+
+# State level Small Multiple ----
+#+ state fig.retina=2, fig.width=12, fig.height=20
+sm <- ggplot(states, aes(x = funding_per_100k, y = total_homeless_per_100k))
+sm <- sm + ggalt:::geom_xspline2(aes(s_open = TRUE, s_shape = 0.6, size = 8))
+sm <- sm + geom_point(aes(group = year), pch = 21, fill = "black", color = "white")
+sm <- sm + geom_label(aes(label = year, y = total_homeless_per_100k, x = funding_per_100k), color = "black", fill = "white", size = 2, family = "Oswald Light", label.size = 0, label.padding = unit(0.1, "lines"), data = filter(states, year %in% c("2007","2014")))
+sm <- sm + scale_x_continuous(labels = scales::dollar, breaks = scales::pretty_breaks(n = 3))
+sm <- sm + scale_y_continuous(labels = scales::comma, breaks = scales::pretty_breaks(n = 5))
+sm <- sm + facet_wrap(~name, scales="free", ncol=4)
+sm <- sm + labs(x = NULL, y = NULL,
+                title="Temporal Scatterplots of US Department of Housing & Urban Development (HUD) Unsheltered (Estimated) Homeless Population\ncontrasted with HUD Continuum of Care Program (CoC) Funding",
+                subtitle="Aggregates calculated from HUD Communities of Care Regional Surveys and CPD Allocations and Awards (normalized per 100k population)",
+                caption="\nHomeless population data from: https://www.hudexchange.info/resource/4832/2015-ahar-part-1-pit-estimates-of-homelessness/\nCoC program Funding data from: https://www.hudexchange.info/grantees/cpd-allocations-awards/")
+
+sm <- sm + theme(text = element_text(family = "Oswald Light", size = 8, color = "grey20"))
+sm <- sm + theme(panel.grid.major.y = element_line(colour = "grey80", size = 0.1))
+sm <- sm + theme(panel.grid.major.x = element_line(colour = "grey80", size = 0.1))
+sm <- sm + theme(legend.position = "none")
+sm <- sm + theme(legend.key = element_blank())
+sm <- sm + theme(legend.background = element_rect(fill = NA))
+sm <- sm + theme(strip.text.y = element_text(angle = 180))
+sm <- sm + theme(axis.text.x = element_text(angle = 0, hjust = 0.5, vjust = 0.5))
+sm <- sm + theme(axis.ticks.y = element_blank())
+sm <- sm + theme(axis.ticks.x = element_blank())
+sm <- sm + theme(panel.margin.y = unit(1, "lines"))
+sm <- sm + theme(axis.line = element_line())
+sm <- sm + theme(panel.border = element_blank())
+sm <- sm + theme(panel.background = element_blank())
+sm <- sm + theme(strip.background = element_blank())
+sm <- sm + theme(plot.background = element_blank())
+sm <- sm + theme(plot.subtitle = element_text(hjust = 0))
+sm <- sm + theme(plot.caption = element_text(hjust = 1, size = 8, vjust = 1))
+sm <- sm + theme(plot.margin=margin(10, 20, 10, 15))
+
+# Bonus plotly national ----
+plot_ly(
+  national,
+  x = scales::dollar(total_funding),
+  y = total_homeless,
+  hoverinfo = "text",
+  text = sprintf(
+    "National CoC Funding = <b>%s</b><br>National Homeless Population = <b>%s</b>",
+    scales::dollar(national$total_funding),
+    scales::comma(national$total_homeless)
+  ),
+  mode = "markers",
+  showlegend = FALSE,
+  marker = list(color = toRGB("grey20"))
+) %>%
+  add_trace(
+    x = total_funding,
+    y = total_homeless,
+    line = list(shape = "spline", color = toRGB("grey20")),
+    showlegend = FALSE,
+    hoverinfo = "none"
+  ) %>%
+  add_trace(
+    text = year,
+    x = total_funding + nudge_x,
+    y = total_homeless + nudge_y,
+    mode = "text",
+    showlegend = FALSE,
+    hoverinfo = "none"
+  ) %>%
+  config(displayModeBar = F) %>%
+  layout(
+    xaxis = list(title = "Total CoC Funding ($)"),
+    yaxis = list(title = "Total Homeless Population<br>"),
+    title = 'Temporal Scatterplot of US Department of Housing & Urban Development (HUD) Unsheltered (Estimated) Homeless Population contrasted with HUD Continuum of Care Program (CoC) Funding',
+    font = list(family = "Oswald-Light", size = 14),
+    autosize = T,
+    width = 1600,
+    height = 1000,
+    margin =list(
+      l = 75,
+      r = 75,
+      b = 100,
+      t = 100,
+      pad = 4
+    )
+  )
+
+# Bonus plotly states ----
+
+plots <- function(df){
+
+  p <- plot_ly(
+    df,
+    x = funding_per_100k,
+    y = total_homeless_per_100k,
+    group = state,
+    hoverinfo = "text",
+    text = sprintf(
+      "Year: <b>%s</b><br>National CoC Funding = <b>%s</b><br>National Homeless Population = <b>%s</b>",
+      states$year,
+      scales::dollar(states$funding_per_100k),
+      scales::comma(states$total_homeless_per_100k)
+    ),
+    mode = "markers",
+    showlegend = FALSE,
+    marker = list(color = toRGB("grey20"))
+  ) %>%
+    add_trace(
+      x = funding_per_100k,
+      y = total_homeless_per_100k,
+      xaxis = list(title = ""),
+      yaxis = list(title = ""),
+      group = state,
+      line = list(shape = "spline", color = toRGB("grey20")),
+      showlegend = FALSE,
+      hoverinfo = "none"
+    ) %>%
+    layout(xaxis = list(title = paste(unique(name))),
+           yaxis = list(title = ""))
+
+  return(p)
+}
+
+plots <- dlply(states, .(state), plots)
+
+options <- list(nrows=12)
+
+do.call(subplot,  c(plots,options)) %>%
+  config(displayModeBar = F) %>%
+  layout(
+    autosize = T,
+    width = 1600,
+    height = 2000,
+    margin =list(l = 50, r = 50, b = 50, t = 50, pad = 4),
+    title = 'Temporal Scatterplot of US Department of Housing & Urban Development (HUD) Unsheltered (Estimated) Homeless Population contrasted with HUD Continuum of Care Program (CoC) Funding',
+    font = list(family = "Oswald-Light", size = 12))
 
 
